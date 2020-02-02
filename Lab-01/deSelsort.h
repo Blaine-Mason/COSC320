4967883d30ded92cf7e9822d292965f5cc632f96
Blaine-Mason <bmason3@gulls.salisbury.edu>
2020-02-01 23:49:16 -0500

    Almost Done

--- Lab-01/deSelsort.h
+++ Lab-01/deSelsort.h
@@ -5,36 +5,28 @@
 void deSelsort(int arr[], int n, bool ascending){
 	//left and right hold the current "state" of the algorithm
 	//eg left = 1, right = n - 2 -> second iteration
-	int minIndex, maxIndex, temp;
-	int left = 0; 
-	int right = n - 1;
-	while(left < right){
-		minIndex = left;
-		maxIndex = right;
-		for(int i = left + 1; i < right; i++){
-			if(arr[i] < arr[minIndex]){
-				minIndex = i;
+	int minIndex, maxIndex, temp, i = 0;
+	while(i <= n-i-1){
+		minIndex = i;
+		maxIndex = n-i-1;
+		for(int j = i + 1; j <= n-i-1; j++){
+			if(arr[j] <= arr[minIndex]){
+				minIndex = j;
 			}
-			if(minIndex != left){
-				temp = arr[left];
-				arr[left] = arr[minIndex];
-				arr[minIndex] = temp; 
+			if(arr[j] >= arr[maxIndex]){
+				maxIndex = j;
 			}
-			for(int j = right - 1; j > left; j--){
-				if(arr[j] > arr[maxIndex]){
-					maxIndex = j;
-				}
-				if(maxIndex != right){
-					temp = arr[right];
-					arr[right] = arr[maxIndex];
-					arr[maxIndex] = temp;
-
-				}
-			}
-
 		}
-	left++;
-	right--;
+		temp = arr[i];
+		arr[i] = arr[minIndex];
+		arr[minIndex] = temp;
+
+
+		temp = arr[n-i-1];
+		arr[n-i-1] = arr[maxIndex];
+		arr[maxIndex] = temp;
+
+		i++;
 	}
 };
 #endif
